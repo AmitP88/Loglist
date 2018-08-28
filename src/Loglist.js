bd0c6dc4bc9e09cd2999aa1f1bb045db626a1a27
Amit Patel <amitpatel_7@msn.com>
2018-08-28 04:20:11 -0700

    received json data from gist (see dev console)

--- src/Loglist.js
+++ src/Loglist.js
@@ -5,6 +5,12 @@ export default class Loglist extends Component {
         super(props);
     }
 
+    componentWillMount() {
+        fetch('https://rawgit.com/AmitP88/a60ab38cb30c150376b548e3e167e2fd/raw/0ba1947a64eb7c6cad2c0dc77b3ff9e52ade1a62/mockData.json')
+            .then(response => response.json())
+            .then(data => console.log(data))
+    }
+
     render() {
         return (
             <div>
