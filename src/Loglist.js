8882f7ac986df077d3dbee8f78e89820664d5a73
Amit Patel <amitpatel_7@msn.com>
2018-08-28 04:32:06 -0700

    created state object for Loglist component and set Logs property to mockData array inside fetch

--- src/Loglist.js
+++ src/Loglist.js
@@ -3,12 +3,22 @@ import React, { Component } from 'react';
 export default class Loglist extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            Logs: []
+        }
     }
 
     componentWillMount() {
         fetch('https://rawgit.com/AmitP88/a60ab38cb30c150376b548e3e167e2fd/raw/0ba1947a64eb7c6cad2c0dc77b3ff9e52ade1a62/mockData.json')
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => {
+                // console.log(data);
+                this.setState({
+                    Logs: [data.mockData]
+                });
+                console.log(this.state.Logs);     
+            })
+
     }
 
     render() {
