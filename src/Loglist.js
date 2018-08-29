06c195c2ca39afdd9ab980c5cbcbcda73f95ef12
Amit Patel <amitpatel_7@msn.com>
2018-08-28 23:00:55 -0700

    resolved warning in console and cleared console of data

--- src/Loglist.js
+++ src/Loglist.js
@@ -13,21 +13,15 @@ export default class Loglist extends Component {
         fetch('https://rawgit.com/AmitP88/a60ab38cb30c150376b548e3e167e2fd/raw/0ba1947a64eb7c6cad2c0dc77b3ff9e52ade1a62/mockData.json')
             .then(response => response.json())
             .then(data => {
-                // console.log(data.mockData);
-
                 let list = [];
                 for(let i = 0; i < data.mockData.length; i++){
                     list.push(
-                        <div className="log">
-                            <ReactJson theme={"hopscotch"}   key={i} src={data.mockData[i]} />                        
+                        <div className="log" key={i}>
+                            <ReactJson theme={"hopscotch"} key={i} src={data.mockData[i]} />                        
                         </div>
-
                     );
                 }
                 this.setState({Logs: list});
-                console.log("state", this.state.Logs);
-
-
             });
 
     }
