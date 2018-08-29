ddd5cacbc3f9d6d32275f021a0e5f4a977fc18c1
Amit Patel <amitpatel_7@msn.com>
2018-08-28 21:38:10 -0700

    rendered each json object inside it's own ReactJson element

--- src/Loglist.js
+++ src/Loglist.js
@@ -13,19 +13,29 @@ export default class Loglist extends Component {
         fetch('https://rawgit.com/AmitP88/a60ab38cb30c150376b548e3e167e2fd/raw/0ba1947a64eb7c6cad2c0dc77b3ff9e52ade1a62/mockData.json')
             .then(response => response.json())
             .then(data => {
-                // console.log(data);
-                this.setState({
-                    Logs: [data.mockData]
-                });
-                console.log(this.state.Logs);     
-            })
+                // console.log(data.mockData);
+
+                let list = [];
+                for(let i = 0; i < data.mockData.length; i++){
+                    list.push(
+                        <div className="container">
+                            <ReactJson theme={"hopscotch"} key={i} src={data.mockData[i]} />                        
+                        </div>
+
+                    );
+                }
+                this.setState({Logs: list});
+                console.log("state", this.state.Logs);
+
+
+            });
 
     }
 
     render() {
         return (
             <div className="Loglist">
-            <ReactJson src={this.state.Logs} />
+                {this.state.Logs}
             </div> 
         )
     }
